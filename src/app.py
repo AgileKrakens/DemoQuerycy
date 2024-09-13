b79643272a8a97afb302082102c5d3e61b2f2828
Gabriel-Fernando-Lima <gabrielfernandoxlr@gmail.com>
2024-09-13 16:36:10 -0300

    Estrutura da página perfil

--- src/app.py
+++ src/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
