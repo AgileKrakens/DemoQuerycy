2875ee9fb35696ffd69f95d6d7a58f30cad19b83
jodijotar <jeyjeyjodi@gmail.com>
2024-09-17 21:45:08 -0300

    crawler_personal_data: Implemented data persistence in JSON format

--- web_crawlers/crawler_personal_data/main.py
+++ web_crawlers/crawler_personal_data/main.py
@@ -1,3 +1,4 @@
+import sys
 import info
 import json
 import re
@@ -22,24 +23,31 @@ print('''
                                      --Agile Krakens                                         
 ''')
 
-url = input('\nurl do perfil do político: ')
+def query(url):
+    dados_string = info.personal_data(url)
 
-dados_string = info.personal_data(url)
+    dados = {
+        'nome': re.search(r'civil:(.*)', dados_string).group(1).strip(),
+        'partido': info.partido(url),
+        'telefone': re.findall(r'\(\d{2}\)\s\d{4}-\d{4}', dados_string),
+        'email': re.search(r'E-mail:(.*)', dados_string).group(1).strip() if re.search(r'E-mail:(.*)', dados_string) else '',
+        'mandatos': info.mandato(url)
+    }
 
-dados = {
-    'nome': re.search(r'civil:(.*)', dados_string).group(1).strip(),
-    'partido': info.partido(url),
-    'telefone': re.findall(r'\(\d{2}\)\s\d{4}-\d{4}', dados_string),
-    'email': re.search(r'E-mail:(.*)', dados_string).group(1).strip(),
-    'mandatos': info.mandato(url)
-}
+    file = f"{dados['nome'].replace(' ', '_').lower()}.json"
+    
+    with open(f"/home/kali/Desktop/DemoQuerycy/src/data/vereadores/{file}", 'w', encoding='utf-8') as json_file:
+        json.dump(dados, json_file, ensure_ascii=False, indent=4)
+        
+    print(f"\n{dados['nome']} data collected!")
 
-#salva os dados em um JSON
-with open('personal_data.json', 'w', encoding='utf-8') as json_file:
-    json.dump(dados, json_file, ensure_ascii=False, indent=4)
+def read_urls():
+    urls = sys.stdin.read().splitlines()
+    
+    for url in urls:
+        try:
+            query(url)
+        except Exception as e:
+            print(f"fail to process {url}: {e}")
 
-print('\n\ndados coletados!')
-                                                                                                                                                                                                    
-                                                                                                                                                                                                    
-
-
+read_urls()
