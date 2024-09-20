a1cfe9a994c359e0dfa9c9e05c51be59a0d1a4e5
GKanjiscuk <gi.kanjiscuk@gmailcom>
2024-09-20 11:01:47 -0300

    remodelação de layout da página home + implementação de carrossel + novo header + responsividade geral

---
+++ src/static/javascript/header-script.js
@@ -0,0 +1,5 @@
+hamburguer = document.querySelector(".hamburguer");
+hamburguer.onclick = function(){
+    navBar = document.querySelector(".nav-bar")
+    navBar.classList.toggle("active")
+}
--- src/static/javascript/index.js
+++
@@ -1,7 +0,0 @@
-const hidePlaceholder = document.getElementById('input-txt');
-hidePlaceholder.addEventListener('focus', function(){
-    this.placeholder = '';
-})
-hidePlaceholder.addEventListener('blur', function(){
-    this.placeholder = 'O que deseja procurar ?';
-});
