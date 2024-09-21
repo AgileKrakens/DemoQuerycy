06d35af083f5b2432b5b0c73ce6690b4bd3360ea
KwMajor <147251890+KwMajor@users.noreply.github.com>
2024-09-21 14:04:22 -0300

    update home.html - mudanças das imagens do carousel

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 @app.route('/perfil')
 def perfil():
     return render_template('perfil.html')
