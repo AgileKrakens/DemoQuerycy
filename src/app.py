578f5e6b2cf92fad5576569dc94e600a23094fd3
jodijotar <jeyjeyjodi@gmail.com>
2024-09-23 21:24:23 -0300

    referencing profile data with JSON

--- src/app.py
+++ src/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+@app.route('/perfil/1')
+def perfil_JSON():
+        with open(f'../src/data/json_files/1.json', 'r') as f:
+            dados = json.load(f)
+        return render_template('perfil.html', perfil=dados)
+        
 @app.route('/')
 def index():
     return render_template('home.html')
-@app.route('/perfil')
-def perfil():
-    return render_template('perfil.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
