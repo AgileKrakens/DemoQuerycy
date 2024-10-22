59cf09d314920e110574ddeeca7dc75ec9fe2128
jodijotar <jeyjeyjodi@gmail.com>
2024-10-22 20:05:54 -0300

    update main.py with the new modules

--- web_crawlers/crawler_personal_data/main.py
+++ web_crawlers/crawler_personal_data/main.py
@@ -23,15 +23,19 @@ print('''
                                      --Agile Krakens                                         
 ''')
 
-def query(url):
+def scrap(url):
     dados_string = info.personal_data(url)
-
+    dados_presenca = info.presence_stats(url)
+    comissoes = info.comissoes(url)
+    
     dados = {
         'nome': re.search(r'civil:(.*)', dados_string).group(1).strip(),
         'partido': info.partido(url),
         'telefone': re.findall(r'\(\d{2}\)\s\d{4}-\d{4}', dados_string),
         'email': re.search(r'E-mail:(.*)', dados_string).group(1).strip() if re.search(r'E-mail:(.*)', dados_string) else '',
-        'mandatos': info.mandato(url)
+        'comissoes': re.findall(r'Cargo: (.*?) \((.*?)\)\nComissão: (.*?)-'
+        'presencas': [x for x in dados_presenca[:dados_presenca.index('Falta')] if x.isdigit()]
+        'ausencias': [x for x in dados_presenca[dados_presenca.index('Falta'):] if x.isdigit()]
     }
 
     file = f"{dados['nome'].replace(' ', '_').lower()}.json"
@@ -46,7 +50,7 @@ def read_urls():
     
     for url in urls:
         try:
-            query(url)
+            scrap(url)
         except Exception as e:
             print(f"fail to process {url}: {e}")
 
