ba226901279693011f0f76cf5c010835abfaaba7
jodijotar <jeyjeyjodi@gmail.com>
2024-10-24 19:18:59 -0300

    Update info_tse.py - structuring data into modules

--- web_crawlers/crawler_personal_data/main.py
+++ web_crawlers/crawler_personal_data/main.py
@@ -1,7 +1,8 @@
 import sys
-import info
 import json
 import re
+import info_camara
+import info_tse
 
 print('''
 
@@ -23,19 +24,23 @@ print('''
                                      --Agile Krakens                                         
 ''')
 
-def scrap(url):
+def scrap(url, url2):
     dados_string = info.personal_data(url)
     dados_presenca = info.presence_stats(url)
     comissoes = info.comissoes(url)
     
     dados = {
         'nome': re.search(r'civil:(.*)', dados_string).group(1).strip(),
-        'partido': info.partido(url),
+        'nome_social':
+        'data_nasc':
+        'ocupação': 
+        'partido': info.partido(url)
         'telefone': re.findall(r'\(\d{2}\)\s\d{4}-\d{4}', dados_string),
         'email': re.search(r'E-mail:(.*)', dados_string).group(1).strip() if re.search(r'E-mail:(.*)', dados_string) else '',
         'comissoes': re.findall(r'Cargo: (.*?) \((.*?)\)\nComissão: (.*?)-'
         'presencas': [x for x in dados_presenca[:dados_presenca.index('Falta')] if x.isdigit()]
         'ausencias': [x for x in dados_presenca[dados_presenca.index('Falta'):] if x.isdigit()]
+        'mandatos': 
     }
 
     file = f"{dados['nome'].replace(' ', '_').lower()}.json"
@@ -44,8 +49,9 @@ def scrap(url):
         json.dump(dados, json_file, ensure_ascii=False, indent=4)
         
     print(f"\n{dados['nome']} data collected!")
+    print(dados)
 
-def read_urls():
+def read_urls(): #Incluir url2 no loop
     urls = sys.stdin.read().splitlines()
     
     for url in urls:
