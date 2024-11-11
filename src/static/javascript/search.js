2112ccf7b153806798b45ef2636dcc92d9c6c753
Gabriel-Fernando-Lima <gabrielfernandoxlr@gmail.com>
2024-11-11 19:42:26 -0300

    Update search bar - Politicos

--- src/static/javascript/search.js
+++ src/static/javascript/search.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const filterBox = document.querySelector(".filter-box");
     const cards = document.querySelectorAll(".card");
+    const noResultsMessage = document.querySelector(".no-results");
 
     filterBox.addEventListener("input", (event) => {
-        const searchText = event.target.value.toLowerCase();
-        
+        const searchText = event.target.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        let hasResults = false;
+
         cards.forEach(card => {
-            const name = card.querySelector(".name").textContent.toLowerCase();
-            if (name.includes(searchText)) {
+            const name = card.querySelector(".name").textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            const descricao = card.querySelector(".descricao").textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            if (name.includes(searchText) || descricao.includes(searchText)) {
                 card.style.display = "block"; // Exibe o card
+                hasResults = true;
             } else {
                 card.style.display = "none";  // Oculta o card
             }
         });
+
+        noResultsMessage.style.display = hasResults ? "none" : "block";
     });
 });
  
