cb9e26f6e7e4d262e74e6b3d387ec432f5dbc8a8
jodijotar <jeyjeyjodi@gmail.com>
2024-11-23 21:22:55 -0300

    Query laws_project into perfil/<autor>

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import json
 import os
+from datetime import datetime
 from itertools import cycle
 from db import get_db_connection
 import graphics
@@ -47,27 +48,47 @@ def json_perfil(nome):
     try:
         with open(f'{JSON_DIR}/{nome}.json', encoding='utf-8') as f:
             dados = json.load(f)
-            total_presença = sum(int(dados['presencas'][i]) for i in range(1, len(dados['presencas']), 2))
+            total_presenca = sum(int(dados['presencas'][i]) for i in range(1, len(dados['presencas']), 2))
             total_ausencia = sum(int(dados['ausencias'][i]) for i in range(1, len(dados['ausencias']), 2))
 
-            porcentagem_presença = round(total_presença * 100 / (total_presença + total_ausencia), 2)
-            porcentagem_ausencia = round(total_ausencia * 100 / (total_presença + total_ausencia), 2)
+            porcentagem_presenca = round(total_presenca * 100 / (total_presenca + total_ausencia), 2)
+            porcentagem_ausencia = round(total_ausencia * 100 / (total_presenca + total_ausencia), 2)
             
-        # geraçãos do gráficos
-        autor = dados['nome']
-        grafico = graphics.gerar_grafico(autor)
-
-        return render_template(
-            'perfil.html',
-            perfil=dados,
-            porcentagem_presença=porcentagem_presença,
-            porcentagem_ausencia=porcentagem_ausencia,
-            grafico=grafico
-        )
+            # Geração do gráfico
+            autor = dados['nome']
+            grafico = graphics.gerar_grafico(autor)
+            
+            # Consulta ao banco de dados - projeto de leis
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            query = "SELECT numero, ano, tema, resumo, data, situacao FROM law_records WHERE autor = %s"
+            cursor.execute(query, (autor,))
+            projetos_de_leis = cursor.fetchall() 
+        
     except FileNotFoundError:
         return "Perfil não encontrado :/", 404
     except Exception as e:
-        return f"ocorreu um erro: {str(e)}", 500 
-
+        return f"Ocorreu um erro: {str(e)}", 500
+    finally:
+        if 'cursor' in locals() and cursor is not None:
+            cursor.close()
+        if 'connection' in locals() and connection is not None:
+            connection.close()
+        
+    return render_template(
+        'perfil.html',
+        perfil=dados,
+        porcentagem_presenca=porcentagem_presenca,
+        porcentagem_ausencia=porcentagem_ausencia,
+        grafico=grafico,
+        projetos_de_leis=projetos_de_leis
+    )
+    
+@app.template_filter('format_date')
+def format_date(value):
+    if isinstance(value, datetime):
+        return value.strftime('%d/%m/%Y')
+    return value
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
